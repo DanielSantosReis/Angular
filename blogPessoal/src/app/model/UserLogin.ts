1e88da7ced3f12bafad8446187cedf8985bf9c11
Daniel <kr.danielreis@gmail.com>
2022-02-22 14:55:11 -0300

    @DanielSantosReis [ Add as models para relacionar com o Servidor :memo: ]

--- blogPessoal/src/app/model/UserLogin.ts
+++ blogPessoal/src/app/model/UserLogin.ts
@@ -1,11 +1,9 @@
-import { Postagem } from "./Postagem"
-
-export class Usuario {
+export class UserLogin {
     public id: number
     public nome: string
     public foto: string
     public usuario: string
     public senha: string
     public tipo: string
-    public postagem: Postagem[]
+    public token: string
 }

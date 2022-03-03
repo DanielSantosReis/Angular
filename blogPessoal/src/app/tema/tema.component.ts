57ff976a6468e0b62a7de7970812287586a4fd37
Daniel <kr.danielreis@gmail.com>
2022-03-03 18:48:21 -0300

    @DanielSantosReis [ Feito mais inserções no projeto :rocket: ]

---
+++ blogPessoal/src/app/tema/tema.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
+import { Tema } from '../model/Tema';
+import { TemaService } from '../service/tema.service';
+
+@Component({
+  selector: 'app-tema',
+  templateUrl: './tema.component.html',
+  styleUrls: ['./tema.component.css']
+})
+export class TemaComponent implements OnInit {
+
+  tema: Tema = new Tema()
+  listaTemas: Tema[]
+
+  constructor(
+    private router: Router,
+    private temaService: TemaService
+  ) { }
+
+  ngOnInit() {
+    window.scroll(0,0);
+    if(environment.token == ''){
+      alert('Seção expirou, faça login novamente')
+      this.router.navigate(['/login'])
+    }
+    this.listarTemas()
+  }
+
+  listarTemas(){
+    this.temaService.getAllTemas().subscribe((resp: Tema[])=>{
+      this.listaTemas = resp
+      console.log(this.tema)
+    })
+  }
+
+  cadastrar(){
+    this.temaService.postTema(this.tema).subscribe((resp: Tema)=>{
+      this.tema = resp;
+      alert("Tema cadastrado!")
+      this.listarTemas()
+      this.tema = new Tema()
+
+    })
+  }
+
+}

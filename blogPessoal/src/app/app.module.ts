375f9b865bb71e9e089b5a2bf92f49a28afa7caf
Daniel <kr.danielreis@gmail.com>
2022-03-07 21:06:09 -0300

    @DanielSantosReis [ Ultimos ajustes feito no Blog :rocket: ]

--- blogPessoal/src/app/app.module.ts
+++ blogPessoal/src/app/app.module.ts
@@ -14,6 +14,9 @@ import { InicioComponent } from './inicio/inicio.component';
 import { TemaComponent } from './tema/tema.component';
 import { TemaEditComponent } from './edit/tema-edit/tema-edit.component';
 import { TemaDeleteComponent } from './delete/tema-delete/tema-delete.component';
+import { PostagemEditComponent } from './edit/postagem-edit/postagem-edit.component';
+import { PostagemDeleteComponent } from './delete/postagem-delete/postagem-delete.component';
+import { UsuarioEditComponent } from './edit/usuario-edit/usuario-edit.component';
 
 @NgModule({
   declarations: [
@@ -26,6 +29,9 @@ import { TemaDeleteComponent } from './delete/tema-delete/tema-delete.component'
     TemaComponent,
     TemaEditComponent,
     TemaDeleteComponent,
+    PostagemEditComponent,
+    PostagemDeleteComponent,
+    UsuarioEditComponent,
   ],
   imports: [
     BrowserModule,
